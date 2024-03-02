b733fed855172939c561e661c179ac5430746c01
couvq <quentincouvelaire@gmail.com>
2024-03-02 11:47:43 -0600

    cleanup app component in host

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
 import Button from './Button'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
